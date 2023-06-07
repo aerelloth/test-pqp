9cedb85f3bcb972164f17db197741d5bf667b2d8
Océane Scheidel <oceane.scheidel@gmail.com>
2023-06-07 10:41:01 +0200

    Mise à jour du layout

--- resources/views/layouts/bootstrap/layout.blade.php
+++ resources/views/layouts/bootstrap/layout.blade.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Plus que PROjection</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <header>
@@ -19,6 +19,5 @@
         <footer>
             @include('layouts.bootstrap._footer')
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     </body>
 </html>
