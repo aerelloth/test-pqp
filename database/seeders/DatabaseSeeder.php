4761d515e41db1bd5433f7903da25234e1405dc2
Océane Scheidel <oceane.scheidel@gmail.com>
2023-06-07 22:00:38 +0200

    Enregistrement des seeders pour les lancer automatiquement avec 'php artisan db:seed'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(ApiConfigurationSeeder::class);
+        $this->call(MoviesSeeder::class);
+        $this->call(PermissionsDemoSeeder::class);
+
     }
 }
