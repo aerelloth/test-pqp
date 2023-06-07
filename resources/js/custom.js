c6ce3dfbc4dc358e0256529e06b1bd00fbf42c70
Océane Scheidel <oceane.scheidel@gmail.com>
2023-06-07 10:39:13 +0200

    Import des packages de Bootstrap et de DataTables pour l'intégration

---
+++ resources/js/custom.js
@@ -0,0 +1,12 @@
+/* IMPORTS */
+//js
+import 'bootstrap';
+import DataTable from 'datatables.net-bs5';
+
+//css
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
+
+/* GLOBAL */
+
